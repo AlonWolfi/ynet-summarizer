afa86ae24879a172f4636be5013bfb8cf87dfa5d
Uri Nissenkorn <Uri.Nissenkorn@gmail.com>
2022-04-01 22:09:21 +0300

    tf-idf model and doc2vec textrank

--- src/models/nlp/summarization/textrank.py
+++ src/models/nlp/summarization/textrank.py
@@ -15,18 +15,29 @@ class TextRankModel(BaseModel):
     def __init__(self, stop_words, type=None, weight=None, N=1):
         self.stop_words = stop_words
         self.type = type # model to use with textrank
+
         self.weight = weight # weight function to use with model
+        # set default weight function
+        if self.weight is None:
+            self.weight = lambda x: x
+
         self.N = N # number of sentences
         self.model = None
         if type == 'word2vec':
             #load word2vec model
             self.model = gensim.models.Word2Vec.load(path.join(PROJECT_DIR, 'word2vec/word2vec.model'), mmap='r')
+        elif type == 'doc2vec':
+            #load word2vec model
+            self.model = gensim.models.Doc2Vec.load(path.join(PROJECT_DIR, 'doc2vec/doc2vec.model'), mmap='r')
+    
     
     def _sentence_similarity(self, sent1, sent2, stopwords=None):
         if self.type is None:
             return self._sentence_similarity_base(sent1,sent2,stopwords)
         elif self.type == 'word2vec':
             return self._sentence_similarity_with_word_embedding(sent1,sent2,stopwords)
+        elif self.type == 'doc2vec':
+            return self._sentence_similarity_with_sentence_embedding(sent1,sent2,stopwords)
         else:
             raise NotImplementedError()
 
@@ -43,9 +54,6 @@ class TextRankModel(BaseModel):
         vector1 = [0] * len(all_words)
         vector2 = [0] * len(all_words)
 
-        # set default weight function
-        if self.weight is None:
-            self.weight = lambda x: x
 
         # get word embeddings
         wv = self.model.wv
@@ -78,7 +86,27 @@ class TextRankModel(BaseModel):
                     if wv.has_index_for(w1):
                         vector2[all_words.index(w1)] = max(self.weight(wv.similarity(w2,w1)), vector2[all_words.index(w1)])
 
+        # for w in sent1:
+        #     if wv.has_index_for(w):
+        #         vector1.append(wv[w])
+            
+        # for w in sent2:
+        #     if wv.has_index_for(w):
+        #         vector2.append(wv[w])
+
+        # vector1 = np.average(vector1, axis=0)
+        # vector2 = np.average(vector1, axis=0)
+
         return 1 - cosine_distance(vector1, vector2)
+
+    def _sentence_similarity_with_sentence_embedding(self, sent1, sent2, stopwords=None):
+        if stopwords is None:
+            stopwords = []
+
+        sent1 = [w.lower() for w in sent1]
+        sent2 = [w.lower() for w in sent2]
+
+        return self.weight(self.model.similarity_unseen_docs(sent1,sent2, epochs=100))
 
     def _sentence_similarity_base(self, sent1, sent2, stopwords=None):
         if stopwords is None:
@@ -153,9 +181,17 @@ class TextRankModel(BaseModel):
             chosen_sentence_idx = self._choose_best_sentence(
                 article_tokenized, article_raw, self.stop_words)
             raw_sentences = article_raw.split('.')
+
+            # build summary
+            summary_tokens = []
+            summary = ""
+            for idx in chosen_sentence_idx:
+                summary_tokens += article_tokenized[idx] + ['.']
+                summary += raw_sentences[idx]
+
             summaries.append({
-                    'summary_tokens': [article_tokenized[idx] for idx in chosen_sentence_idx][0],
-                    'summary': [raw_sentences[idx] for idx in chosen_sentence_idx][0]
+                    'summary_tokens': summary_tokens,
+                    'summary': summary
                 })
         return pd.DataFrame(
             data=summaries,
